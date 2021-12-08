e60628414c9382b0ec55c34eea7af742af63bf93
Sylvestre <pasytchangwa@gmail.com>
2021-12-08 23:07:08 +0200

    Create the App component with react-router.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import Greeting from './components/Greeting';
+
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Greeting />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
 
 export default App;
